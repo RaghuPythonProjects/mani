961ad1b8733f6c5fb69705d45c6b135749b54a71
RaghuPythonProjects <118695122+RaghuPythonProjects@users.noreply.github.com>
2024-05-13 17:55:00 +0530

    Update test_query.sql
    proof column refernce changed to fact_asset_scan_vulnerability_instance table

--- test_query.sql
+++ test_query.sql
@@ -1,5 +1,5 @@
 SELECT
-    DISTINCT favf.asset_id AS "Asset ID",
+    DISTINCT da.asset_id AS "Asset ID",
     da.ip_address AS "Asset IP Address",
     da.host_name AS "Asset Name",
     dt.tag_name AS "Asset Location",
@@ -8,7 +8,7 @@ SELECT
     dv.cvss_score AS "CVSS Score",
     dv.riskscore AS "Vulnerability Risk Score",
     dv.description AS "Vulnerability Description",
-    htmlAsText(favf.proof) AS "Vulnerability Proof",
+    htmlAsText(fasvi.proof) AS "Vulnerability Proof",
     htmlAsText(ds.fix) AS "Vulnerability Solution",
     dos.description AS "Operating System",
     dos.asset_type AS "Asset Type",
@@ -19,17 +19,16 @@ SELECT
     dvc.category_name AS "Vulnerability Category",
     dvc.category_id AS "Vulnerability Category ID",
     dv.nexpose_id AS "Vulnerability ID"
-FROM fact_asset_vulnerability_finding favf
-JOIN fact_asset_vulnerability_age fava ON favf.vulnerability_id = fava.vulnerability_id
-JOIN dim_asset da ON favf.asset_id = da.asset_id
-JOIN dim_vulnerability dv ON favf.vulnerability_id = dv.vulnerability_id
-JOIN dim_vulnerability_reference dvr ON favf.vulnerability_id = dvr.vulnerability_id
-JOIN dim_asset_operating_system daos ON favf.asset_id = daos.asset_id
+FROM dim_asset da
+JOIN fact_asset_scan_vulnerability_instance fasvi ON da.asset_id = fasvi.asset_id
+JOIN fact_asset_vulnerability_age fava ON fasvi.vulnerability_id = fava.vulnerability_id
+JOIN dim_vulnerability dv ON fasvi.vulnerability_id = dv.vulnerability_id
+JOIN dim_vulnerability_reference dvr ON fasvi.vulnerability_id = dvr.vulnerability_id
+JOIN dim_asset_operating_system daos ON da.asset_id = daos.asset_id
 JOIN dim_operating_system dos ON daos.operating_system_id = dos.operating_system_id
-JOIN dim_fingerprint_source dfs ON daos.fingerprint_source_id = dfs.fingerprint_source_id
 JOIN dim_vulnerability_solution dvs ON dv.vulnerability_id = dvs.solution_id
 JOIN dim_solution ds ON dvs.solution_id = ds.solution_id
 JOIN dim_vulnerability_category dvc ON dv.vulnerability_id = dvc.vulnerability_id
-JOIN dim_tag_asset dat ON da.asset_id = dat.asset_id
-JOIN dim_tag dt ON dat.tag_id = dt.tag_id
+JOIN dim_tag_asset dta ON da.asset_id = dta.asset_id
+JOIN dim_tag dt ON dta.tag_id = dt.tag_id
 WHERE dv.cvss_score >= 7
