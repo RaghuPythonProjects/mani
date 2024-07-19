6e4ee6fe0f58bcc90acd84520cff3e86a33d7651
RaghuPythonProjects <118695122+RaghuPythonProjects@users.noreply.github.com>
2024-07-20 02:52:56 +0530

    Create config.py

---
+++ wiz_jql/config.py
@@ -0,0 +1,7 @@
+WIZ_ENV = None  # Set to "gov" or "fedramp", if applicable.
+WIZ_CLIENT_ID = 'x624ue26z5bclpg6mocpasj4nz5tkqav5u7fxcngwjqfecjowxhxg'
+WIZ_CLIENT_SECRET = 'JwlY8uATOijqLXnkGNAwz4g4RKa9C2sLBZJNCRZaHkbKYmBAbT8o4Ho1LYu3GCf0'
+WIZ_API_PROXY = None
+PROJECT_ID = "50264363-66ad-5ca2-b622-75df754e5439"
+OUTPUT_CSV = 'reports.csv'
+QUERY_PATH = 'query.txt'
